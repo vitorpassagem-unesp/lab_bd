75649dcec0aec4003a9df250ca4e9588b2f8094e
vitorpassagem-unesp <vitor.passagem@unesp.br>
2025-11-30 02:42:30 -0300

    faz cleanup de arquivos inuteis

--- test_list_models_gemini.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# Configure sua chave Gemini
-API_KEY = "AIzaSyAefXQCKBMksD0IBXI88FfpfCoLyLNrKj0"
-genai.configure(api_key=API_KEY)
-
-print("Modelos disponíveis na API Gemini:")
-for model in genai.list_models():
-    print("-", model)
